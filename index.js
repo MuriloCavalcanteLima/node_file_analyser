5fbb37edf42bf94e5bfb0e64d4b46320d238a3d7
Murilo Cavalcante Lima <muricav@hotmail.com>
2025-04-15 05:11:12 +0000

    exercise upload

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+const removeUploadedFiles = require('multer/lib/remove-uploaded-files');
 require('dotenv').config()
 
 var app = express();
@@ -7,11 +11,31 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(!req.file) res.status(400).json({message: 'file not uploaded'})
 
+  res.json({
+    "name": req.file.filename,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
